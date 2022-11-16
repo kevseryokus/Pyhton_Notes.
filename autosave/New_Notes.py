8ee95b99387ac0f504428b9d8bffa104d678c32f
KEVSER YOKUS <kevseryokuss@gmail.com>
2022-11-17 01:01:38 +0300

    My new sep again remember notes

---
+++ autosave/New_Notes.py
@@ -0,0 +1,38 @@
+#başlangıc seviye,yeniden hatırlamaya yonelik giris notlarım
+x = "Kevser"
+y = 15
+z = "15"
+
+print(x)
+print(y)
+print(z)
+
+print(x,y,z)
+print("jkedwjlwsvwkl",z,y,"kev",2,2.5)
+
+print("T","B","M","M",sep = ".")
+print("T","B","M","M")
+print("T","B","M","M",sep = "88888....")
+
+print("T","B","M","M",sep = "")
+print("K","N","M","L",sep = " ")
+print("T","B","M","M",sep=" ",end = "\n")
+print("kevser","yokuş")
+
+print("T","B","M","M",sep=" ",end = "")
+print("kevser","yokuş")
+
+print("T","B","M","M",sep=" ",end = " ")
+print("kevser","yokuş")
+
+print("T","B","M","M",sep=" ",end = "-----")
+print("kevser","yokuş")
+
+print("T","B","M","M",sep=" ",end = "-----")
+
+print("T","B","M","M",sep="**")
+#sep aralara tırnak içinde yazılanı koyar.En sona koymaz. En sona da end ile koydururuz.
+print("T","B","M","M",sep="**",end="**")
+
+print("kivi","portakal")
+print("elma","armut")
